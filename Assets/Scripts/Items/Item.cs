803115050cb51ae6b30c0ed914e83ebf43e8b2d3
BerezkovN <nikita.berezkof@gmail.com>
2021-11-06 19:44:22 +0200

    Interactable

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -2,22 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : Interactable
+public class Item : 
+    Interactable
 {
     public GameObject Key;
     public bool KeyPrompt = true;
 
     private GameObject createdKey;
 
-    protected override void OnTriggerEnter()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         createdKey = Instantiate(Key);
         createdKey.transform.position = this.gameObject.transform.position + Vector3.up;
     }
 
-    protected override void OnTriggerExit()
+    private void OnTriggerExit2D(Collider2D collision)
     {
         GameObject.Destroy(createdKey);
     }
-
 }
