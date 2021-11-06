2d88f78d414b22163eb309d533bbedcf447abb09
nullsoftware <tumyrthebot@gmail.com>
2021-11-06 17:09:00 +0200

    added possibility to scroll camera to edges.

--- Assets/Scripts/CameraView.cs
+++ Assets/Scripts/CameraView.cs
@@ -8,6 +8,7 @@ public class CameraView : MonoBehaviour
     [SerializeField] private float _dampTime = 0.15f;
     [SerializeField] private float _offsetX;
     [SerializeField] private float _offsetY;
+    [SerializeField, Range(0, 0.8f)] private float _borderSize = 0.05f;
     [SerializeField] private Transform _target;
 
     private Camera _camera;
@@ -21,6 +22,19 @@ public class CameraView : MonoBehaviour
 
     private void LateUpdate()
     {
+        float edgeOffsetX = 0;
+        float edgeOffsetY = 0;
+
+        float borderX = _borderSize * Screen.width;
+        float borderY = _borderSize * Screen.height;
+        
+        Vector3 mousePos = Input.mousePosition;
+
+        if (mousePos.x <= borderX || mousePos.x >= Screen.width - borderX)
+            edgeOffsetX = Screen.width / 2 > mousePos.x ? -1 : 1;
+        if (mousePos.y <= borderY || mousePos.y >= Screen.height - borderY)
+            edgeOffsetY = Screen.height / 2 > mousePos.y ? -1 : 1;
+
         if (_target)
         {
             Vector3 point = _camera.WorldToViewportPoint(_target.position);
@@ -28,6 +42,10 @@ public class CameraView : MonoBehaviour
             Vector3 destination = transform.position + delta;
             destination.x += _offsetX;
             destination.y += _offsetY;
+
+            destination.x += edgeOffsetX;
+            destination.y += edgeOffsetY;
+
             transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, _dampTime);
         }
 
