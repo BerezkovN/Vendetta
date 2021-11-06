0ed3a1c57d2952ad3618de6d93be07a487344157
malatindez <53783400+malatindez@users.noreply.github.com>
2021-11-06 21:23:10 +0200

    Update Interactables

--- Assets/Scripts/Items/Interactable.cs
+++ Assets/Scripts/Items/Interactable.cs
@@ -4,5 +4,85 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
+    [SerializeField] Key Key;
+    [SerializeField] GameObject? CreatedKey;
+
+    private bool _hidden = false;
+    private SpriteRenderer _keyRenderer;
+    private void Awake()
+    {
+        Initialize();
+        CreatedKey.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(_hidden)
+        {
+            return;
+        }
+        CreatedKey.SetActive(true);
+        OnTriggerEnter2DInternal(collision);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (_hidden)
+        {
+            return;
+        }
+        CreatedKey.SetActive(false);
+        OnTriggerExit2DInternal(collision);
+    }
+    
+    [ContextMenu("Initialize")]
+    public void Initialize()
+    {
+        GameObject? buf = CreatedKey;
+        if (Key != null)
+        {
+            CreatedKey = new GameObject(Key.name);
+            CreatedKey.transform.parent = gameObject.transform;
+            _keyRenderer = CreatedKey.AddComponent<SpriteRenderer>();
+            _keyRenderer.sprite = Key.SpriteTexture;
+            if (buf != null)
+            {
+                CreatedKey.transform.position = buf.transform.position;
+                DestroyImmediate(buf);
+            }
+            else
+            {
+                CreatedKey.transform.position = gameObject.transform.position + Vector3.up;
+            }
+        }
+        else if (buf != null)
+        {
+            Destroy(buf);
+        }
+        
+    }
+
+    public void Click()
+    {
+        _keyRenderer.sprite = Key.SpriteTexturePressed;
+        ClickInternal();
+    }
+    public void ClickExit()
+    {
+        _keyRenderer.sprite = Key.SpriteTexture;
+        ClickExitInternal();
+    }
+
+    protected void HideKey()
+    {
+        _hidden = true;
+        CreatedKey.SetActive(false);
+    }
+
+    protected virtual void OnTriggerEnter2DInternal(Collider2D collision) { /* intenionally unimplemented */ }
+    protected virtual void OnTriggerExit2DInternal(Collider2D collision) { /* intenionally unimplemented */ }
+    protected virtual void ClickInternal() { /* intenionally unimplemented */ }
+    protected virtual void ClickExitInternal() { /* intenionally unimplemented */ }
+
+
 
 }
--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -5,19 +5,4 @@ using UnityEngine;
 public class Item : 
     Interactable
 {
-    public GameObject Key;
-    public bool KeyPrompt = true;
-
-    private GameObject createdKey;
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        createdKey = Instantiate(Key);
-        createdKey.transform.position = this.gameObject.transform.position + Vector3.up;
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        GameObject.Destroy(createdKey);
-    }
 }
