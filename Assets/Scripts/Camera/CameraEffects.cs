12eb9713cff4552d661611e4e15ebfbf59d22265
malatindez <53783400+malatindez@users.noreply.github.com>
2021-11-07 15:02:33 +0200

    Update CameraEffects.cs

--- Assets/Scripts/Camera/CameraEffects.cs
+++ Assets/Scripts/Camera/CameraEffects.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Experimental.Rendering.Universal;
+
 
 public class CameraEffects : MonoBehaviour
 {
-    [SerializeField] UnityEngine.UI.Image _canvasBlackSquare;
+    [SerializeField] private Light2D _light;
+    [SerializeField] private Volume _postProcessing;
+
+    private Bloom _bloom;
+    private ColorAdjustments _colorAdjustments;
+    private Vignette _vignette;
+
     private void Start()
     {
-        
+        if(_light == null || _postProcessing == null)
+        {
+            throw new System.ArgumentNullException($"neither {nameof(_light)} nor {nameof(_postProcessing)} shouldn't be null");
+        }
+        _postProcessing.profile.TryGet(out _bloom);
+        _postProcessing.profile.TryGet(out _colorAdjustments);
+        _postProcessing.profile.TryGet(out _vignette);
     }
 
     private void Update()
@@ -16,6 +32,7 @@ public class CameraEffects : MonoBehaviour
     }
     public void SetExitFade(float t)
     {
-        _canvasBlackSquare.color = new Color(0, 0, 0, t);
+        _colorAdjustments.postExposure.value = 5*Mathf.Log10(1+t) + 1/(1+t) - 1 - 4 * t  * t;
+        _vignette.intensity.value = t;
     }
 }
